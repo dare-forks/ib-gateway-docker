1d6f57a6d8c8b455ee71b6ce874293f2119467b6
degerhan <5803485+degerhan@users.noreply.github.com>
2025-08-13 13:20:42 -0400

    switch rdesktop:ubuntu-xfce to webtop:ubuntu-xfce-kasm

--- stable/tws-scripts/start_session.sh
+++ stable/tws-scripts/start_session.sh
@@ -27,16 +27,16 @@ if [ -n "$START_SCRIPTS" ]; then
 	run_scripts "$HOME/$START_SCRIPTS"
 fi
 
-# open xfce session
-echo ".> Openning Xrdp session"
-_out=$(echo "${_PASS}" | xrdp-sesrun -s 127.0.0.1 -F 0 abc)
-unset _PASS #unset
-_display=$(echo "$_out" | grep -e '^ok' | cut -d ' ' -f 3 | cut -d '=' -f 2)
-if [ -n "$_display" ]; then
-	DISPLAY=$_display
-	export DISPLAY
-	echo ".> Xrdp started on DISPLAY=${DISPLAY}"
-fi
+# # open xfce session
+# echo ".> Openning Xrdp session"
+# _out=$(echo "${_PASS}" | xrdp-sesrun -s 127.0.0.1 -F 0 abc)
+# unset _PASS #unset
+# _display=$(echo "$_out" | grep -e '^ok' | cut -d ' ' -f 3 | cut -d '=' -f 2)
+# if [ -n "$_display" ]; then
+# 	DISPLAY=$_display
+# 	export DISPLAY
+# 	echo ".> Xrdp started on DISPLAY=${DISPLAY}"
+# fi
 
 # setting permissions
 echo ".> Setting permissions for ${TWS_PATH} and ${IBC_PATH}"
